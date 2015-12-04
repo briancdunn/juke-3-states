30feb1bf2c2bf29e6a1407183687cadfaef281f7
Alan <ajohnson88@gmail.com>
2015-12-04 16:11:39 -0500

    up to bonus round

--- browser/js/module.js
+++ browser/js/module.js
@@ -1,24 +1,35 @@
 var app = angular.module('juke', ['ui.router']);
 
 app.config(function($stateProvider) {
-  $stateProvider.state('albums', {
-    templateUrl: '/templates/albums.html',
-    url: '/albums',
-    controller: 'AlbumsCtrl'
-  });
-  $stateProvider.state('albums.album', {
-    templateUrl: '/templates/album.html',
-    url: '/albums/:id',
-    controller: 'AlbumCtrl'
-  });
-  $stateProvider.state('artists',{
-    templateUrl: '/templates/artists.html',
-    url: '/artists',
-    controller: 'ArtistsCtrl'
-  });
-  $stateProvider.state('artist',{
-    templateUrl: '/templates/artist.html',
-    url: '/artists/:id',
-    controller: 'ArtistCtrl'
-  })
+  $stateProvider
+    .state('albums', {
+      templateUrl: '/templates/albums.html',
+      url: '/albums',
+      controller: 'AlbumsCtrl'
+    })
+    .state('album', {
+      templateUrl: '/templates/album.html',
+      url: '/album/:id',
+      controller: 'AlbumCtrl'
+    })
+    .state('artists',{
+      templateUrl: '/templates/artists.html',
+      url: '/artists',
+      controller: 'ArtistsCtrl'
+    })
+    .state('artist',{
+      templateUrl: '/templates/artist.html',
+      url: '/artists/:id',
+      controller: 'ArtistCtrl'
+    })
+    .state('artist.albums',{
+      templateUrl: '/templates/artists/albums.html',
+      url: '/artists/:id/albums',
+      controller: 'ArtistCtrl'
+    })
+    .state('artist.songs',{
+      templateUrl: '/templates/artists/songs.html',
+      url: '/artists/:id/songs',
+      controller: 'ArtistCtrl'
+    })
 })
