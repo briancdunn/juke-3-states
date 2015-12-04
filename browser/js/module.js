34235fa0dbd9e5d9ca056e058aa6d774687408c6
Brian Dunn <briancollierdunn@gmail.com>
2015-12-04 15:35:37 -0500

    replaced original view stuff with states and templates

--- browser/js/module.js
+++ browser/js/module.js
@@ -1,1 +1,24 @@
-var app = angular.module('juke', []);
+var app = angular.module('juke', ['ui.router']);
+
+app.config(function($stateProvider) {
+  $stateProvider.state('albums', {
+    templateUrl: '/templates/albums.html',
+    url: '/albums',
+    controller: 'AlbumsCtrl'
+  });
+  $stateProvider.state('albums.album', {
+    templateUrl: '/templates/album.html',
+    url: '/albums/:id',
+    controller: 'AlbumCtrl'
+  });
+  $stateProvider.state('artists',{
+    templateUrl: '/templates/artists.html',
+    url: '/artists',
+    controller: 'ArtistsCtrl'
+  });
+  $stateProvider.state('artist',{
+    templateUrl: '/templates/artist.html',
+    url: '/artists/:id',
+    controller: 'ArtistCtrl'
+  })
+})
